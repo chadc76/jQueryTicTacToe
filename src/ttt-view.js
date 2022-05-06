1e265516480f878d111b2f42f5ecca7a5c7f2f2c
Chad Cunningham <chadcunningham7676@gmail.com>
2022-05-06 17:09:32 -0400

    Completed Project

--- src/ttt-view.js
+++ src/ttt-view.js
@@ -7,9 +7,44 @@ class View {
     this.bindEvents();
   }
 
-  bindEvents() {}
+  bindEvents() {
+    this.$el.on("click", "li", (event => {
+      const $square = $(event.currentTarget);
+      this.makeMove($square);
+    }));
+  }
 
-  makeMove($square) {}
+  makeMove($square) {
+    const pos = $square.data('pos');
+    const currentPlayer = this.game.currentPlayer;
+    
+    try {
+      this.game.playMove(pos);
+    } catch (e) {
+      alert("This " + e.msg.toLowerCase());
+      return;
+    }
+    
+    $square.addClass(currentPlayer);
+
+    if (this.game.isOver()) {
+      this.$el.off("click");
+      this.$el.addClass("game-over");
+
+      const winner = this.game.winner();
+      const $figcaption = $("<figcaption>");
+
+      if (winner) {
+        this.$el.addClass(`winner-${winner}`);
+        $figcaption.html(`You win, ${winner}!`);
+      } else {
+        $figcaption.html("It's a draw!");
+      }
+
+      this.$el.append($figcaption);
+    }
+
+  }
 
   setupBoard() {
     const $ul = $("<ul>");
